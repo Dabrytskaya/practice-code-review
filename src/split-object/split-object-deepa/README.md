2b804cce60df555c7a2c1dbf4d7512268210ec27
deepa@hyf123 <deepamlavil@gmail.com>
2021-07-29 17:58:21 +0200

    solution folder is added for split-object

---
+++ src/split-object/split-object-deepa/README.md
@@ -0,0 +1,23 @@
+# challenge name: strategy name
+
+Splits an object into multiple objects with one key/value pair each.
+
+## Strategy
+
+Using 'forEach' for iterating through each key, and pushing with key/value pair to new array variable
+
+## Implementation
+
+Taking the keys only to a new variable 'keys', and for each key in the 'keys' pushing the 'key : value' to a new array 'separatedArray'
+
+## Use Cases
+
+---
+
+## Inspiration
+
+<!--
+  was there any code, blog post, video, ... that inspired your solution?
+  there's nothing wrong with adapting other people's code, just give them credit!
+  and say how it inspired your solution.
+-->
---
+++ src/split-object/split-object-deepa/split-object.js
@@ -0,0 +1,36 @@
+// eslint-disable-next-line spellcheck/spell-checker
+/**
+ * Splits an object into multiple objects with one key/value pair each.
+ * The new objects are returned in an array.
+ *
+ * This function has no side-effects, the argument object is not modified.
+ *
+ * @param {object} [toSeparate={}] - The object to split into key/value pairs.
+ * @returns {Array} Returns a new array with one entry for each key/value pair.
+ * @example
+ *
+ * splitObject({ a: 1, b: 2, c: 3 });
+ * // -> [{ a: 1 }, { b: 2 }, { c: 3 }]
+ *
+ * @example
+ *
+ * splitObject({ name: 'robs', age: 25, tall: true, userName: 'stor' });
+ * // -> [{ name: 'robs }, { age: 25 }, { tall: true }, { userName: 'stor' }]
+ *
+ * @example
+ *
+ * splitObject({});
+ * // -> []
+ */
+
+export const splitObject = (toSeparate = {}) => {
+  // new array of objects with key/value pairs
+  const separatedArray = [];
+  // assigning the keys only to new variable
+  const keys = Object.keys(toSeparate);
+  // for each key in the 'keys' pushing the 'key : value' to 'separatedArray'
+  keys.forEach((key) => {
+    separatedArray.push({ [key]: toSeparate[key] });
+  });
+  return separatedArray;
+};
