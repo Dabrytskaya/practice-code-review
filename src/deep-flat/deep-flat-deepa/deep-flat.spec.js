705844f48e324145d0c92d3836574ac89389db26
deepa-thomas <78364046+deepa-thomas@users.noreply.github.com>
2021-07-31 20:51:39 +0200

    updated with new test case
    updated

--- src/deep-flat/deep-flat-deepa/deep-flat.spec.js
+++ src/deep-flat/deep-flat-deepa/deep-flat.spec.js
@@ -2,8 +2,8 @@ import { deepFlat } from './deep-flat.js';
 
 describe('Converts a nested array into a single array', () => {
   it('array of numbers', () => {
-    const expected = ['a', 'b', 'c', 'd', 'e'];
-    const received = deepFlat(['a', ['b', [['c'], ['d']], 'e']]);
+    const expected = [1, 2, 3, 4, 5];
+    const received = deepFlat([1, [2, [3, [4]], 5]]);
     expect(received).toEqual(expected);
   });
   it('array of strings', () => {
